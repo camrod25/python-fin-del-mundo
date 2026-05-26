470f92416fa6a4da655994da217d75506ea8268c
Cristian <piki_moreno@hotmail.com>
2026-05-25 22:24:38 -0400

    Mi primera app en streamlit

---
+++ quibo_parce.py
@@ -0,0 +1,11 @@
+
+import streamlit as st
+
+st.title("Quibo Parce!!")
+st.subheader("Bienvenido al curso python para el fin del mundo")
+
+st.write("""
+Esta es mi primera app en streamlit
+""")
+
+st.info("Pronto esta APP va a predecir asteroides peligrosos para el planeta tierra")
